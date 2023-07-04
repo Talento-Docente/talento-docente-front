bc38e668f374fca681bb8a4a2a024c6fd425319f
YoelMee <yoelaguirre92@gmail.com>
2023-07-03 22:22:54 -0400

    inputsearch

--- src/components/layouts/NavbarTopLandingLayout.vue
+++ src/components/layouts/NavbarTopLandingLayout.vue
@@ -24,20 +24,31 @@
 .landing-layout
   a-layout#layout-menu
     .header
-      a-layout-header(:style="{ position: 'fixed', zIndex: 1, width: '100%' }")
-        a-space(:style="{float: 'right'}")
-          a-button
-            UserAddOutlined
-            span Registrarse
-          a-button(href="/login")
-            UserOutlined
-            span Ingresar
-        a-menu(
-          v-model:selectedKeys="selectedKeys"
-          theme="dark"
-          mode="horizontal")
-          a-menu-item(key="1") Nav 1
-          a-menu-item(key="2") Nav 2
+      a-layout-header(:style="{ position: 'fixed', zIndex: 1, width: '100%'}")
+        a-row(type="flex" justify="space-around" align="middle" :gutter="[20,20]")
+          a-col(:xl="{ span: 6 }")
+              a-menu(
+                v-model:selectedKeys="selectedKeys"
+                theme="dark"
+                mode="horizontal")
+                a-menu-item(key="1") Nav 1
+                a-menu-item(key="2") Nav 2
+
+          a-col(:xl="{ span: 6 }")
+            a-input-search(v-model:value="value"
+              placeholder="input search text"
+              enter-button
+              @search="onSearch")
+              span sas
+              
+          a-col(:xl="{ span: 6 }")
+            a-space(:style="{float: 'right'}")
+              a-button
+                UserAddOutlined
+                span Registrarse
+              a-button(href="/login")
+                UserOutlined
+                span Ingresar
     .content.margin-top__30
       a-layout-content(:style="{ padding: '0 50px', marginTop: '64px'}")
         a-breadcrumb(:style="{ margin: '16px 0' }")
