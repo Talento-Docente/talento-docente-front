b64355f04e1895da43d4483f481855de8b0b40d5
YoelMee <yoelaguirre92@gmail.com>
2023-07-04 03:01:57 -0400

    avance-landing

--- src/components/layouts/NavbarTopLandingLayout.vue
+++ src/components/layouts/NavbarTopLandingLayout.vue
@@ -2,10 +2,16 @@
   import { defineComponent, ref } from 'vue';
   /** External dependencies */
   import { RouterView } from 'vue-router'
+  /** Interfaces */
+  import type { MenuProps } from 'ant-design-vue';
   /** Icons */
   import {
     UserOutlined,
-    UserAddOutlined
+    UserAddOutlined,
+    DownOutlined,
+    BankOutlined,
+    QuestionCircleOutlined
+    
   } from '@ant-design/icons-vue';
 
   export default defineComponent({
@@ -16,7 +22,10 @@
     },
     components: {
     UserOutlined,
-    UserAddOutlined
+    UserAddOutlined,
+    DownOutlined,
+    BankOutlined,
+    QuestionCircleOutlined
   },
   });
 </script>
@@ -25,30 +34,66 @@
   a-layout#layout-menu
     .header
       a-layout-header(:style="{ position: 'fixed', zIndex: 1, width: '100%'}")
-        a-row(type="flex" justify="space-around" align="middle" :gutter="[20,20]")
-          a-col(:xl="{ span: 6 }")
+        a-row(type="flex" justify="space-around" align="middle")
+          a-col(flex="auto")
+            //- Logo
+            a-avatar(src="src/assets/talento-docente/default.png" size="large")
+          //- Central menu options
+          a-col(:xl="{ span: 16 }")
               a-menu(
                 v-model:selectedKeys="selectedKeys"
+                v-model:openKeys="openKeys"
                 theme="dark"
                 mode="horizontal")
-                a-menu-item(key="1") Nav 1
-                a-menu-item(key="2") Nav 2
+                //- a-menu-item(key="1")
+                //-   a(href=" ")
+                //-   span Home
+                a-menu-item(key="pro") Profesionales
+                a-menu-item(key="emp") Empleadores
+                a-sub-menu(key="est" title="Establecimientos")
+                  a-sub-menu(key="est-nor" title="Zona Norte")
+                    a-menu-item(key="15") XV Region
+                    a-menu-item(key="1") I Region
+                  a-sub-menu(key="est-cen" title="Zona Central")
+                    a-sub-menu(key="est-v" title="V Region")
+                      a-menu-item(key="Valparaiso") Valparaiso
+                      a-menu-item(key="Viña") Viña
+                    a-sub-menu(key="est-met" title="Region Metropolitana")
+                      a-menu-item(key="Conchalí") Conchalí
+                  a-sub-menu(key="est-sur" title="Zona Sur")
+                   a-menu-item(key="6") VI Region
+                   a-menu-item(key="7") VII Region
+                //- a-menu-item(key="Help")
+                //-   span   Ayuda 
+                //-   QuestionCircleOutlined
 
-          a-col(:xl="{ span: 6 }")
-            a-input-search(v-model:value="value"
-              placeholder="input search text"
-              enter-button
-              @search="onSearch")
-              span sas
-              
-          a-col(:xl="{ span: 6 }")
+          //- Right Options menu Buttons - submenu/Register-Login
+          a-col(flex="auto")
             a-space(:style="{float: 'right'}")
-              a-button
-                UserAddOutlined
-                span Registrarse
+              a-dropdown
+                template(#overlay)
+                  a-menu
+                    a-menu-item(key="7")
+                      UserAddOutlined
+                      span  Profesional
+                    a-menu-item(key="8")
+                      BankOutlined
+                      span  Empleador
+                a-button
+                  span Registrarse
+                  DownOutlined
               a-button(href="/login")
                 UserOutlined
                 span Ingresar
+          a-col(flex="auto")
+            a-menu(
+              v-model:selectedKeys="selectedKeys"
+              theme="dark"
+              mode="horizontal")
+              a-menu-item(key="Help" :style="{background: 'none'}")
+                span   Ayuda 
+                QuestionCircleOutlined
+    //- Body Content
     .content.margin-top__30
       a-layout-content(:style="{ padding: '0 50px', marginTop: '64px'}")
         a-breadcrumb(:style="{ margin: '16px 0' }")
--- src/views/landing/JobsApplicantView.vue
+++ src/views/landing/JobsApplicantView.vue
@@ -1,23 +1,43 @@
-<script setup>
+<script lang="ts">
 /** External dependencies */
-import { defineComponent, ref } from 'vue'
+  import { defineComponent, ref } from 'vue'
+  import { 
+  SettingOutlined,
+  EditOutlined, 
+  EllipsisOutlined 
+  } from '@ant-design/icons-vue'
+
+  export default defineComponent({
+    components: {
+      SettingOutlined,
+      EditOutlined, 
+      EllipsisOutlined 
+    },
+
+  })
 </script>
 <template lang="pug">
 .landing
   .filter
-    a-row(:gutter="[20, 20]")
-      a-col(:xl="{ span: 6 }")
-        a-form-item(label="Buscar por")
-          a-input(placeholder="Nombre, dirección, teléfono...")
-
-      a-col
-        a-form-item
-          a-button(type="primary")
-            span Buscar
-
-      a-col
-        a-form-item
-          a-button(type="primary")
-            span Crear
-
+    a-row( type="flex" justify="space-around")
+      a-col(:xl="{ span: 8 }")
+        a-input-search(v-model:value="value"
+            placeholder="Buscar Profesion ..."
+            enter-button
+            @search="onSearch")
+            span sas
+  .jobsApplicants.margin-top__30
+    a-card(hoverable style="width: 300px")
+      template(#cover)
+        img(
+          alt="example"
+          src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png")
+      template(#actions)
+        setting-outlined(key="setting")
+        edit-outlined(key="edit")
+        ellipsis-outlined(key="ellipsis")
+      a-card-meta(title="Card title" description="This is the description")
+        template(#avatar)
+          a-avatar(src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png")
+    
 </template>
