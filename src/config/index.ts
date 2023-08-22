d9205790e78b31e4a8625885c23abc1faa1a2e8d
Felipe Vega Herrera <vega.herrera.felipe@gmail.com>
2023-08-22 13:42:21 -0400

    fix: establishment validate dni

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 export default {
   nodeEnv: import.meta.env.NODE_ENV || 'development',
-  urlApi: import.meta.env.VUE_APP_URL_API || 'http://localhost:3000',
-  selfUrl: import.meta.env.VUE_APP_SELF_URL || 'http://localhost:8080'
+  urlApi: import.meta.env.VITE_URL_API || 'http://localhost:3000',
+  selfUrl: import.meta.env.VITE_SELF_URL || 'http://localhost:8080'
 }
